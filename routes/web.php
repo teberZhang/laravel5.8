925f99413ae8e4e02921cdc33533dc01ef7ba00e
teber_zhang <teber_zhang@163.com>
2019-07-25 14:44:03 +0800

    进阶系列 —— 缓存

--- routes/web.php
+++ routes/web.php
@@ -180,7 +180,7 @@ Route::get('redis-publish', function () {
     Redis::publish('users.me001', json_encode(['id' => rand(10,100)]));
 });
 
-Route::get('sqlBuilder', 'Stage\OrderController@ship');
+Route::get('sqlBuilder', 'Stage\CacheController@tag');
 
 
 Auth::routes();
